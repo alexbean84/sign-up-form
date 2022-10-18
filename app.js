c6cb425f066c3eb9e7be3e7a2170b8dbe24b15df
alexbean84 <alexbean84@gmail.com>
2022-10-19 02:43:50 +0300

    Define the constants for the HTML's inputs

---
+++ app.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const email = document.getElementById('email');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs() {
+    
+}
